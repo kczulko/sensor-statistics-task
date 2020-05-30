3df137fb8bd5d28963be4361a1b6ba7b7ebec5aa
Karol Czulkowski <karol.czulkowski@gmail.com>
2020-05-30 22:32:01 +0200

    Add test for program[F]

---
+++ src/test/scala/com/github/kczulko/sst/ProgramSpec.scala
@@ -0,0 +1,63 @@
+package com.github.kczulko.sst
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.Resources
+import cats.effect.ContextShift
+import cats.effect.ConcurrentEffect
+import cats.effect.IO
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class ProgramSpec extends AnyFlatSpec
+    with Matchers
+    with ProgramSpecExpectedResults {
+
+  implicit val cs = IO.contextShift(global)
+
+  def testInterpreter(
+    expectedNumberOfProcessedFiles: Int,
+    expectedResults: Map[String,SensorMeasurements]
+  ): ResultsInterpreter.ResultInterpreter[IO] = {
+    case (numOfFiles, results) =>
+
+      val programResults = results.mapValues { value =>
+        val data = value
+          .data
+          .map(measurement => (measurement.avg, measurement.min, measurement.max))
+        (data, value.failedCount, value.totalMeasurements)
+      }.toMap
+
+      val modifiedExpectedResults = expectedResults.mapValues { value =>
+        val data = value
+          .data
+          .map(measurement => (measurement.avg, measurement.min, measurement.max))
+        (data, value.failedCount, value.totalMeasurements)
+      }.toMap
+
+      numOfFiles shouldEqual expectedNumberOfProcessedFiles
+      programResults shouldEqual modifiedExpectedResults
+      IO.unit    
+  }
+
+  def test(params: TestParameters) =
+    "program[F]" should s"properly analyze ${params.suiteName}" in {
+      import params._
+
+      Main.program[IO](
+        s"src/test/resources/${params.suiteName}",
+        testInterpreter(expectedNumberOfFiles, expectedResults)
+      ).compile
+       .drain
+       .unsafeRunSync()
+    }
+
+  it should behave like test(singleFileSingleSensor)
+  it should behave like test(singleFileSingleSensorOnlyNaNs)
+  it should behave like test(singleFileInvalidEntries)
+  it should behave like test(multipleFilesSingleSensor)
+  it should behave like test(multipleFilesMultipleSensorsNaNs)
+  it should behave like test(multipleFilesInvalidEntries)
+  it should behave like test(nonExistingDirectory)
+  it should behave like test(emptyDirectory)
+}
---
+++ src/test/scala/com/github/kczulko/sst/ProgramSpecExpectedResults.scala
@@ -0,0 +1,72 @@
+package com.github.kczulko.sst
+
+final case class TestParameters(
+  suiteName: String,
+  expectedNumberOfFiles: Int,
+  expectedResults: Map[String, SensorMeasurements]
+)
+
+trait ProgramSpecExpectedResults {
+
+  def singleFileSingleSensor = TestParameters(
+    "singleFileSingleSensor",
+    1,
+    Map("1" -> SensorMeasurements(Some(MeasurementState(0, 22, 61, 44.333332f)), 0, 6))
+  )
+
+  def singleFileSingleSensorOnlyNaNs = TestParameters(
+    "singleFileSingleSensorOnlyNaNs",
+    1,
+    Map("3" -> SensorMeasurements(None, 2, 2))
+  )
+
+  def singleFileInvalidEntries = TestParameters(
+    "singleFileInvalidEntries",
+    1,
+    Map(
+      "3" -> SensorMeasurements(Some(MeasurementState(0, 10, 10, 10)), 0, 1),
+      "measure-error" -> SensorMeasurements(None, 2, 2)
+    )
+  )
+
+  def multipleFilesSingleSensor = TestParameters(
+    "multipleFilesSingleSensor",
+    2,
+    Map(
+      "1" -> SensorMeasurements(Some(MeasurementState(0, 10, 80, 45)), 0, 8),
+    )
+  )
+
+  def multipleFilesMultipleSensorsNaNs = TestParameters(
+    "multipleFilesMultipleSensorsNaNs",
+    3,
+    Map(
+      "1" -> SensorMeasurements(Some(MeasurementState(0, 10, 40, 21.25f)), 0, 4),
+      "2" -> SensorMeasurements(Some(MeasurementState(0, 20, 50, 36.666668f)), 0, 3),
+      "3" -> SensorMeasurements(Some(MeasurementState(0, 70, 70, 70)), 1, 2),
+      "4" -> SensorMeasurements(None, 1, 1),
+    )
+  )
+
+  def multipleFilesInvalidEntries = TestParameters(
+    "multipleFilesInvalidEntries",
+    2,
+    Map(
+      "1" -> SensorMeasurements(Some(MeasurementState(0, 50, 60, 55)), 0, 2),
+      "2" -> SensorMeasurements(Some(MeasurementState(0, 50, 80, 66.666664f)), 0, 3),
+      "measure-error" -> SensorMeasurements(None, 4, 4),
+    )
+  )
+
+  def nonExistingDirectory = TestParameters(
+    "jfkdl;sa,nsdfndskla;",
+    0,
+    Map.empty
+  )
+
+  def emptyDirectory = TestParameters(
+    "emptyDirectory",
+    0,
+    Map.empty
+  )
+}
